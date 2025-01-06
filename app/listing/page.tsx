507665b5d062cd164456dd58f3f44168a0b655f6
yusrilprayoga-code <124210006@student.upnyk.ac.id>
2025-01-06 23:04:29 +0700

    Update Content Website

--- app/listing/page.tsx
+++ app/listing/page.tsx
@@ -7,7 +7,9 @@ const MapPage = () => {
     <div className="font-[family-name:var(--font-geist-sans)] max-w-[100vw] overflow-x-hidden">
       <NavBar />
       <main className="max-w-7xl mx-auto px-4 py-8">
-        <h1 className="text-3xl font-bold text-center mb-8">Location</h1>
+        <h1 className="text-3xl font-bold text-center mb-8">
+          Peta Desa Maduretno
+        </h1>
         <div className="flex justify-center">
           <GoogleMap width="1200" height="600" />
         </div>
